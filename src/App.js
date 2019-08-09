aa11e71c6c5b197ba54996afdfa45906b8cb52e5
tcg <tommy.george@youversion.com>
2019-08-09 12:23:42 -0500

    Basic test case

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// React Draft WYSIWYG + HTML To DraftJS for directly opening HTML:
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import htmlToDraft from 'html-to-draftjs';
+import { EditorState, ContentState } from 'draft-js';
+
+
+// Pulled into a function to make this easier. Based on example from docs at:
+// https://github.com/jpuri/html-to-draftjs
+const htmlToDraftBlocks = (html) => {
+  const blocksFromHtml = htmlToDraft(html);
+  const { contentBlocks, entityMap } = blocksFromHtml;
+  const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
+  const editorState = EditorState.createWithContent(contentState);
+  return editorState;
+};
+
+
 function App() {
+
+  // Using html-to-draftjs, per documented examples at
+  //  https://github.com/jpuri/html-to-draftjs
+  // and
+  //  https://jpuri.github.io/react-draft-wysiwyg/#/docs?_k=jjqinp
+  const HTML_WITHOUT_FIGURE = '<p><strong>this seems safe to load</strong></p>';
+  const HTML_WITH_FIGURE = '<p><figure></figure><strong>this seems safe to load</strong></p>';
+
+  // Let's set `editorState` based on either the content with
+  // or without HTML. Change the variable below and refresh the page
+  // to see the result!
+  const editorState = htmlToDraftBlocks(HTML_WITH_FIGURE);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Editor
+        defaultEditorState={editorState}
+      />
+
     </div>
   );
 }
